320021da21bf45b8889e547cb0a5c3a0a45f6dba
Ignacio Peluffo <ipeluffo@gmail.com>
2016-12-07 18:11:58 +0000

    Print variables properties in basic Log Service

--- business_rules/service/log_service.py
+++ business_rules/service/log_service.py
@@ -6,5 +6,13 @@ class LogService(object):
         super(LogService, self).__init__()
         self.logger = logging.getLogger(__name__)
 
+    def _fetch_public_properties(self, obj):
+        return [name for name in dir(obj) if not name.startswith('_')]
+
     def log_rule(self, rule, conditions_met, action_triggered, defined_variables):
-        self.logger.info('rule={} - conditions = {} - actions = {}'.format(rule, conditions_met, action_triggered))
+        variables_public_props = self._fetch_public_properties(defined_variables)
+
+        self.logger.info('rule={} - conditions = {} - actions = {} - defined_variables = {}'.format(rule,
+                                                                                                    conditions_met,
+                                                                                                    action_triggered,
+                                                                                                    variables_public_props))
