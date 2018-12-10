e82eb11280e0dd95e6c1b3d91c1d34f293904114
Ignacio Peluffo <ipeluffo@gmail.com>
2018-12-10 12:10:49 +0000

    unpinned six version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ description = 'Python DSL for setting up business intelligence rules that can be
 install_requires = [
     'pytz>=2016.10',
     'typing',
-    'six==1.11.0',
+    'six',
 ]
 
 setuptools.setup(
